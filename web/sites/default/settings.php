ed74d5e9494d623658f1cecb3cca5d538dac3a21
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2019-04-02 12:03:53 +0200

    Remove minimal profile from settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -837,4 +837,3 @@ $local_settings_file = $app_root . '/' . $site_path . '/settings.local.php';
 if (file_exists($local_settings_file)) {
   include $local_settings_file;
 }
-$settings['install_profile'] = 'minimal';
