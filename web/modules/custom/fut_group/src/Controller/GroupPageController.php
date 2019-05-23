2118fb6883d6d3cc57398d1f25193086f1895e4a
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2019-05-23 14:29:19 +0200

    72 : Add collection functionality

--- web/modules/custom/fut_group/src/Controller/GroupPageController.php
+++ web/modules/custom/fut_group/src/Controller/GroupPageController.php
@@ -4,8 +4,8 @@ namespace Drupal\fut_group\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\group\Entity\Group;
+use Drupal\taxonomy\Entity\Term;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * Discover page controller.
@@ -50,6 +50,27 @@ class GroupPageController extends ControllerBase {
 
   public function aboutTitle(Group $group) {
     return $group->label();
+  }
+
+  public function collections(Group $group) {
+    $view_builder = $this->entityTypeManager()->getViewBuilder('group');
+    return $view_builder->view($group, 'fut_about');
+  }
+
+  public function addCollection(Group $group) {
+    $taxonomy_term = Term::create([
+      'fut_related_group' => $group->id(),
+      'vid' => 'fut_collections',
+    ]);
+    return $this->entityFormBuilder()->getForm($taxonomy_term, 'default');
+  }
+
+  public function editCollection(Group $group, Term $term) {
+    return $this->entityFormBuilder()->getForm($term, 'default');
+  }
+
+  public function deleteCollection(Group $group, Term $term) {
+    return $this->entityFormBuilder()->getForm($term, 'delete');
   }
 
 }
---
+++ web/modules/custom/fut_group/src/Routing/RouteSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\fut_group\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+
+    if ($route = $collection->get('view.fut_group_posts.page_group_posts')) {
+      // Allow the views to have an optional argument.
+      $route->setDefault('collection', 'all');
+    }
+
+    if ($route = $collection->get('view.fut_group_events.page_group_events')) {
+      // Allow the views to have an optional argument.
+      $route->setDefault('collection', 'all');
+    }
+
+    if ($route = $collection->get('view.fut_group_library.page_group_library')) {
+      // Allow the views to have an optional argument.
+      $route->setDefault('collection', 'all');
+    }
+  }
+
+}
