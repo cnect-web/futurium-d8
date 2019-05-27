dcef1d2acd28bb942d3ab2fdeaaebd4a3287b2ad
Adriano Dias <adrianodias1994@gmail.com>
2019-05-27 14:51:53 +0200

    67 : make extra field work with new EC Webtools Map formatter

--- web/modules/custom/fut_content/src/Plugin/ExtraField/Display/EventMap.php
+++ web/modules/custom/fut_content/src/Plugin/ExtraField/Display/EventMap.php
@@ -3,11 +3,7 @@
 namespace Drupal\fut_content\Plugin\ExtraField\Display;
 
 use Drupal\Core\Entity\ContentEntityInterface;
-use Drupal\Core\Extension\ModuleHandlerInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Render\RendererInterface;
 use Drupal\extra_field\Plugin\ExtraFieldDisplayBase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Example Extra field Display.
@@ -20,149 +16,29 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   }
  * )
  */
-class EventMap extends ExtraFieldDisplayBase implements ContainerFactoryPluginInterface {
-
-
-  /**
-   * The module handler service.
-   *
-   * @var \Drupal\Core\Extension\ModuleHandlerInterface
-   */
-  protected $moduleHandler;
-
-
-  /**
-   * The renderer.
-   *
-   * @var \Drupal\Core\Render\RendererInterface
-   */
-  protected $renderer;
-
-  /**
-   * Constructs a ExtraFieldDisplayFormattedBase object.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
-   *   The module handler.
-   * @param \Drupal\Core\Render\RendererInterface $renderer
-   *   The renderer service.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, ModuleHandlerInterface $module_handler, RendererInterface $renderer) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-
-    $this->moduleHandler = $module_handler;
-    $this->renderer = $renderer;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration, $plugin_id, $plugin_definition,
-      $container->get('module_handler'),
-      $container->get('renderer')
-    );
-  }
+class EventMap extends ExtraFieldDisplayBase {
 
   /**
    * {@inheritdoc}
    */
   public function view(ContentEntityInterface $entity) {
 
+    $elements = [];
     // First we check if we have an address on event.
     if ($this->eventHasLocation($entity) && $this->eventShowMap($entity)) {
-
-      $path = base_path() . $this->moduleHandler->getModule('fut_content')->getPath();
-      $path .= '/js/fut_ec_map.js';
-
-      $json = '{"service": "map", "version": "2.0", "custom": "' . $path . '"}';
-
-      $map_identifier = $entity->bundle() . '-' . $entity->id();
-
-      $map_properties = [
-        '#type' => 'html_tag',
-        '#tag' => 'script',
-        '#attributes' => [
-          'type' => 'application/json',
+      // Here we get the field with coordinates that will generate a map.
+      $original_field = $entity->fut_event_coordinates->view([
+        'type' => 'webtools_geofield_formatter',
+        'label' => 'hidden',
+        'settings' => [
+          'map_view_mode' => 'map_description_default',
         ],
-        '#value' => $json,
-        '#attached' => [
-          'library' => [
-            'fut_content/webtools-smart-loader',
-          ],
-          'drupalSettings' => [
-            'fut_content' => [
-              'ec_map' => [
-                $map_identifier =>[
-                  'featureCollection' => $this->prepareEventMarker($entity),
-                  'center' => $this->getEventCoordinates($entity),
-                ],
-              ],
-            ],
-          ],
-        ],
-      ];
+      ]);
 
-      $elements = [
-        '#theme' => 'ec_map',
-        '#attributes' => [
-          'id' => 'ec-map-' . $map_identifier,
-          'data-map-id' => $map_identifier,
-          'class' => [
-            'ec-map',
-          ],
-        ],
-        'map_properties' => $map_properties,
-      ];
+      $elements[] = $original_field;
 
-      return $elements;
     }
-  }
-
-  /**
-   * Creates properties array needed to display marker in map.
-   *
-   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
-   *   Event entity.
-   *
-   * @return array
-   *   Properties array with marker.
-   *
-   * @throws \Exception
-   */
-  private function prepareEventMarker(ContentEntityInterface $entity) {
-    $address_element = $entity->fut_event_address->view();
-    $address = $this->renderer->render($address_element);
-
-    $coordinates = array_reverse($this->getEventCoordinates($entity));
-
-    $feature = [
-      'properties' => [
-        'name' => $entity->label(),
-        'description' => $address,
-      ],
-      'type' => 'Feature',
-      'geometry' => [
-        'type' => 'point',
-        'coordinates' => $coordinates,
-      ],
-    ];
-
-    $feature_collection = [
-      'type' => 'FeatureCollection',
-      'features' => [
-        $feature,
-      ],
-    ];
-
-    return $feature_collection;
-
+    return $elements;
   }
 
   /**
@@ -195,22 +71,6 @@ class EventMap extends ExtraFieldDisplayBase implements ContainerFactoryPluginIn
       return FALSE;
     }
     return TRUE;
-  }
-
-  /**
-   * Get array with coordinates.
-   *
-   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
-   *   Event entity.
-   *
-   * @return array
-   *   Array with coordinates (lat/lon).
-   */
-  private function getEventCoordinates(ContentEntityInterface $entity) {
-    return [
-      $entity->fut_event_coordinates->lat,
-      $entity->fut_event_coordinates->lon,
-    ];
   }
 
 }
