27a00c3cf2dd9af69bbae40b5661e8af374a596e
Adriano Dias <adrianodias1994@gmail.com>
2019-07-15 14:56:01 +0200

    101 : WIP - finish hoo_views_data to discribe fut_activity table

---
+++ web/modules/custom/fut_activity/src/FutActivityViewsOperations.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace Drupal\fut_activity;
+
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+class FutActivityViewsOperations implements ContainerInjectionInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new FutActivityViewsOperations instance.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * Discribes fut_activity data table to Views module.
+   *
+   * @return array
+   *   An associative array describing the data structure.
+   */
+  public function getViewsData() {
+    $data['fut_activity']['table']['group'] = $this->t('Fut Activity');
+    $data['fut_activity']['table']['base'] = [
+      'field' => 'activity_id',
+      'title' => $this->t('Fut Activity'),
+      'help' => $this->t('Fut Activity table keeps track of entity activity.'),
+      'weight' => -10,
+    ];
+
+    $data['fut_activity']['table']['join'] = $this->getImplicitRelations();
+
+    $data['fut_activity'] = $data['fut_activity'] + $this->getRelationsFields();
+
+    $data['fut_activity']['activity'] = [
+      'title' => $this->t('Activity'),
+      'help' => $this->t('Entity activity value.'),
+      'field' => [
+        'id' => 'numeric',
+        'click sortable' => TRUE,
+      ],
+      'filter' => [
+        'id' => 'numeric',
+      ],
+      'argument' => [
+        'id' => 'numeric',
+      ],
+      'sort' => [
+        'id' => 'standard',
+      ],
+    ];
+
+    $data['fut_activity']['created'] = [
+      'title' => $this->t('Created'),
+      'help' => $this->t('when record was created'),
+      'field' => [
+        'id' => 'date',
+        'click sortable' => TRUE,
+      ],
+      'filter' => [
+        'id' => 'date',
+      ],
+      'argument' => [
+        'id' => 'date',
+      ],
+      'sort' => [
+        'id' => 'standard',
+      ],
+    ];
+
+    $data['fut_activity']['changed'] = [
+      'title' => $this->t('last changed'),
+      'help' => $this->t('Last time record was changed'),
+      'field' => [
+        'id' => 'date',
+        'click sortable' => TRUE,
+      ],
+      'filter' => [
+        'id' => 'date',
+      ],
+      'argument' => [
+        'id' => 'date',
+      ],
+      'sort' => [
+        'id' => 'standard',
+      ],
+    ];
+
+    return $data;
+  }
+
+  /**
+   * Get all existing EntityActivityTrackers.
+   *
+   * @return \Drupal\fut_activity\Entity\EntityActivityTrackerInterface[]
+   *   Array containing all trackers config entities.
+   */
+  protected function getTrackers() {
+    return $this->entityTypeManager->getStorage('entity_activity_tracker')->loadMultiple();
+  }
+
+  /**
+   * Get implicit relations to Trackers entity types.
+   *
+   * @return array
+   *   Associative array describing relation when tracked entity type is the base table.
+   */
+  protected function getImplicitRelations() {
+    $id_names = [];
+    foreach ($this->getTrackers() as $tracker) {
+      $id_names[$tracker->getTargetEntityType()] = $this->entityTypeManager->getStorage($tracker->getTargetEntityType())->getEntityType()->getKey('id');
+    }
+    $joins = [];
+
+    foreach ($id_names as $entity_type => $id_name) {
+      $joins[$entity_type.'_field_data'] = [
+        'left_field' => $id_name,
+        'field' => 'entity_id',
+      ];
+    }
+    return $joins;
+  }
+
+  /**
+   * Get relations to Trackers.
+   *
+   * @return array
+   *   Associative array describing relation fields.
+   */
+  protected function getRelationsFields() {
+    if (count($this->getTrackers())) {
+      $data = [];
+      foreach ($this->getTrackers() as $tracker) {
+        $entity_type = $this->entityTypeManager->getStorage($tracker->getTargetEntityType())->getEntityType();
+
+        $data[$entity_type->getKey('id')] = [
+          'title' => $this->t('Entity ID'),
+          'help' => $this->t('Tracked Entity ID.'),
+          'relationship' => [
+            'id' => 'standard',
+            'title' => $this->t('@entity_type', ['@entity_type' => $entity_type->getLabel()]),
+            'help' => $this->t('Relate activity to the @entity_type  that is being tracked.',['@entity_type' => $entity_type->getLabel()]),
+            'handler' => 'views_handler_relationship',
+            'base' => $entity_type->getBaseTable(),
+            'base field' => $entity_type->getKey('id'),
+            'field' => 'entity_id',
+            'label' => $this->t('Entity: @entity_type',['@entity_type' => $entity_type->getLabel()]),
+          ],
+          'field' => [
+            'id' => 'numeric',
+            'click sortable' => TRUE,
+          ],
+          'filter' => [
+            'id' => 'numeric',
+          ],
+          'argument' => [
+            'id' => 'numeric',
+          ],
+          'sort' => [
+            'id' => 'standard',
+          ],
+        ];
+      }
+      return $data;
+    }
+  }
+}
