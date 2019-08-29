f9eda25917ba5e77c3427f8187154d04ffd0b252
Horacio <horaciolopes@gmail.com>
2019-08-29 14:14:09 +0200

    Test ci

--- RoboFile.php
+++ RoboFile.php
@@ -9,6 +9,10 @@ use Robo\Tasks as RoboTasks;
 use Robo\Config\Config;
 use Consolidation\Config\Loader\YamlConfigLoader;
 use Consolidation\Config\Loader\ConfigProcessor;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\Input;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Yaml\Yaml;
 use DrupalFinder\DrupalFinder;
 use Symfony\Component\Filesystem\Filesystem;
 use Webmozart\PathUtil\Path;
@@ -23,19 +27,12 @@ class RoboFile extends RoboTasks {
   use \Boedah\Robo\Task\Drush\loadTasks;
 
   private $env;
-
   private $fs;
-
   private $binDir;
-
   private $projectRoot;
-
   private $drupalRoot;
-
   private $config;
-
   private $defaultOp = 'cs,unit,behat';
-
   private $defaultPaths = 'web/modules/custom,web/themes/contrib/blellow';
 
   /**
@@ -60,14 +57,69 @@ class RoboFile extends RoboTasks {
       $dotenv->load();
       $this->env = getenv();
     }
+  }
 
+  /**
+   * Add default options.
+   *
+   * @param \Symfony\Component\Console\Command\Command $command
+   *   Command object.
+   *
+   * @hook option
+   */
+  public function defaultOptions(Command $command) {
+    $command->addOption('config', 'c', InputOption::VALUE_REQUIRED, 'Configuration file to be used instead of default `robo.yml.dist`.', 'robo.yml');
+    $command->addOption('override', 'o', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Configuration value(s) to be overridden. Format: "path.to.key:value"', []);
+  }
+
+  /**
+   * Command initialization.
+   *
+   * @param \Symfony\Component\Console\Input\Input $input
+   *   Input object.
+   *
+   * @hook pre-init
+   */
+  public function initializeConfiguration(Input $input) {
+    // Initialize configuration objects.
     $config = new Config();
     $loader = new YamlConfigLoader();
     $processor = new ConfigProcessor();
-    $processor->extend($loader->load("{$this->projectRoot}/config.yml"));
-    $config->import($processor->export());
-    $this->config = $config;
+    // Extend and import configuration.
+    $processor->add($config->export());
+    $processor->extend($loader->load('config.yml.dist'));
+    $processor->extend($loader->load('config.yml'));
+    $processor->extend($loader->load($input->getOption('config')));
 
+    // Replace tokens in final configuration file.
+    $export = $processor->export();
+    // Load configuration with unprocessed tokens just to be able to access
+    // the static values as dot chained names.
+    $config->import($export);
+    array_walk_recursive($export, function (&$value, $key) use ($config) {
+      if (is_string($value)) {
+        preg_match_all('/![A-Za-z_\-.]+/', $value, $matches);
+        foreach ($matches[0] as $match) {
+          $config_key = substr($match, 1, strlen($match));
+          $value = str_replace($match, $config->get($config_key), $value);
+        }
+      }
+    });
+    // Reimport the config, this time with the tokens replaced.
+    $config->import($export);
+    // Process command line overrides.
+    foreach ($input->getOption('override') as $override) {
+      $override = (array) Yaml::parse($override);
+      $override_key = key($override);
+      $override_value = array_shift($override);
+      if (!empty($override_value)) {
+        $config->set($override_key, $override_value);
+      }
+      else {
+        $this->statusMessage("Empty override value: \"{$override_key}\", using default value instead.", 'warn');
+      }
+    };
+    $this->config = $config;
   }
 
   /**
@@ -751,6 +803,12 @@ class RoboFile extends RoboTasks {
    */
   protected function getLocalSettingsFile() {
     return "{$this->getLocalSettingsFolder()}/settings.php";
+  }
+
+  public function dumpConfig() {
+
+
+    var_dump($this->config->export());//get('theme.path'));
   }
 
 }
